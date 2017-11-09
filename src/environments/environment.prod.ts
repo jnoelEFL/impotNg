b360064fdb7c67316a5d6b1c2173ef7cda33ff8a
Julien Noel <j.noel@lefebvre-sarrut.eu>
2017-11-09 17:30:58 +0100

    build & deploy to Github pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
+import { dateVersion } from './version';
+
 export const environment = {
   env: 'production',
   production: true,
+  dateVersion,
   hmr: false
 };
--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,1 +1,1 @@
-export const dateVersion = '09/11/2017 à 13:26:13';
+export const dateVersion = '09/11/2017 à 17:29:20';
